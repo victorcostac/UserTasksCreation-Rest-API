792ccf6cb7d8d6d51bdc14be3b3a0b7f5f5a8e51
victorcostac <107112671+victorcostac@users.noreply.github.com>
2023-10-15 17:20:31 -0300

    fork deploy

--- todolist/src/main/java/br/com/victorcosta/todolist/user/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.victorcosta.todolist.user;
-
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<UserModel, UUID>{
-    UserModel findByUsername(String username);
-}
