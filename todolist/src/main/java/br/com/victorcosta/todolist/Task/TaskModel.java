7f7f0c468e0b472e41ce823f56164f1b3c67559c
Victor Costa <victorcostadecoura@gmail.com>
2023-10-15 02:26:28 -0300

    ExceptionHandlerController created

--- todolist/src/main/java/br/com/victorcosta/todolist/Task/TaskModel.java
+++ todolist/src/main/java/br/com/victorcosta/todolist/Task/TaskModel.java
@@ -40,6 +40,11 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDate createdAt;
 
-
-    
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("O campo title deve conter no máximo 50 caracteres.");
+        }
+        this.title = title;
+    }
+ 
 }
